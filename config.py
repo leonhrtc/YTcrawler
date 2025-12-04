7786a9a5086347276c892f8a9ff72ca3d727a37e
leonhrtc <93948184+leonhrtc@users.noreply.github.com>
2025-12-04 20:07:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # ====================================================
 # YouTube Data API Key
-# 修改这里即可，无需改任何其他文件
 # ====================================================
 
 YOUTUBE_API_KEY = "AIzaSyAC7rQjcSTsVFMPOLHSXOFV4_9OCg-DOiU"
+
